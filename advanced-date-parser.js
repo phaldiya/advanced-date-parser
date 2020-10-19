454f3fe3b3cacd367049677fe86af09ea08b42ad
Pradeep K Haldiya <phaldiya@ucdavis.edu>
2020-10-19 06:28:09 -0700

    fix(regexp): correct the issue of regexp

--- advanced-date-parser.js
+++ advanced-date-parser.js
@@ -1,8 +1,38 @@
-"use strict";
-
 const moment = require('moment');
-const supportedDateFormates = ["YYYY-MM-DD", "MM/DD/YYYY", "MM-DD-YYYY", "DD/MM/YYYY", "YYYY-MM-DD H:mm", "YYYY-MM-DDTH:mm:ss.sssZ", "YYYY-MM-DDTH:mm:ssZ", "YYYY-MM-DD h:mm:ss a",
-  moment.ISO_8601, "X", "x", "MMM Do, YYYY", "MMM Do YYYY", "MMMM Do, YYYY", "MMMM Do YYYY", "dddd, MMMM DD, YYYY h:mm a", "ddd, MMMM DD, YYYY h:mm a"];
+
+const supportedDateFormates = [
+  'YYYY-MM-DD',
+  'MM/DD/YYYY',
+  'MM-DD-YYYY',
+  'DD/MM/YYYY',
+  'YYYY-MM-DD H:mm',
+  'YYYY-MM-DDTH:mm:ss.sssZ',
+  'YYYY-MM-DDTH:mm:ssZ',
+  'YYYY-MM-DD h:mm:ss a',
+  moment.ISO_8601,
+  'X',
+  'x',
+  'MMM Do, YYYY',
+  'MMM Do YYYY',
+  'MMMM Do, YYYY',
+  'MMMM Do YYYY',
+  'dddd, MMMM DD, YYYY h:mm a',
+  'ddd, MMMM DD, YYYY h:mm a',
+  'L',
+  'LL',
+  'LLL',
+  'LLLL',
+];
+
+const strictDateRegex = function() {
+  return new RegExp('date', 'ig');
+};
+const unixDateRegex = function() {
+  return new RegExp('^\\d{10}(\\.\\d{0,3})?$');
+};
+const millisecondsRegex = function() {
+  return new RegExp('^\\d{13}$');
+};
 
 const model = {
   dateParser: (strict) => {
@@ -10,29 +40,34 @@ const model = {
       model.parse(req.body, strict);
       model.parse(req.query, strict);
       next();
-    }
+    };
   },
 
   bodyDateParser: (strict) => {
     return (req, res, next) => {
       model.parse(req.body, strict);
       next();
-    }
+    };
   },
 
   queryDateParser: (strict) => {
     return (req, res, next) => {
       model.parse(req.query, strict);
       next();
-    }
+    };
   },
 
   parse: (object, strict) => {
     if (typeof object !== 'object' || object === null) return;
 
-    for (let key in object) {
+    for (const key in object) {
       // avoid circular reference infinite loop & skip inherited properties
-      if (object === object[key] || !Object.prototype.hasOwnProperty.call(object, key) || typeof object[key] !== 'object') continue;
+      if (
+        object === object[key] ||
+        !Object.prototype.hasOwnProperty.call(object, key) ||
+        typeof object[key] !== 'object'
+      )
+        continue;
       model.parse(object[key], strict);
     }
 
@@ -43,24 +78,24 @@ const model = {
 
   _findAndReplace: (object, strict) => {
     strict = strict !== false;
-    let dateRegex = new RegExp("date", "ig");
 
     Object.keys(object).forEach((key) => {
-      if (typeof object[key] !== 'boolean' && isNaN(object[key]) && (!strict || !isNaN(key) || dateRegex.test(key)) && moment(object[key], supportedDateFormates, true).isValid()) {
+      if (
+        typeof object[key] !== 'boolean' &&
+        isNaN(object[key]) &&
+        (!strict || !isNaN(key) || strictDateRegex().test(key)) &&
+        moment(object[key], supportedDateFormates, true).isValid()
+      ) {
         object[key] = moment(object[key], supportedDateFormates, true).toDate();
-      } else if (!isNaN(object[key]) && (!strict || dateRegex.test(key))) {
-        let unixDateRegex = new RegExp("^\\d{10}(\\.\\d{0,3})?$");
-        let millisecondsRegex = new RegExp("^\\d{13}$");
-
-        if (unixDateRegex.test(object[key])) {
+      } else if (!isNaN(object[key]) && (!strict || strictDateRegex().test(key))) {
+        if (unixDateRegex().test(object[key])) {
           object[key] = moment.unix(object[key]).toDate();
-        } else if (millisecondsRegex.test(object[key])) {
-          object[key] = moment(object[key]).toDate();
+        } else if (millisecondsRegex().test(object[key])) {
+          object[key] = moment(object[key], 'x').toDate();
         }
       }
     });
-  }
-
+  },
 };
 
 module.exports = model;
--- test/advanced-date-parser.spec.js
+++ test/advanced-date-parser.spec.js
@@ -1,44 +1,41 @@
-"use strict";
-
-const expect = require("chai").expect;
+const { expect } = require('chai');
 const moment = require('moment');
 const dateParser = require('../advanced-date-parser');
 
 describe('Date Parser', () => {
-
   it('should parse the object with date params', () => {
-    let req = {
+    const req = {
       query: {
-        term: "express",
-        startDate: "2017-10-01"
-      }
+        term: 'express',
+        startDate: '2017-10-01',
+      },
     };
 
     dateParser.parse(req.query);
 
     expect(req.query).to.deep.equal({
-      term: "express",
-      startDate: moment("2017-10-01").toDate()
+      term: 'express',
+      startDate: moment('2017-10-01').toDate(),
     });
 
     expect(req.query.startDate instanceof Date).to.equal(true);
   });
 
   it('should parse the object with date params in not strict mode', () => {
-    let req = {
+    const req = {
       query: {
-        term: "express",
-        from: "2017-10-01",
-        to: "2017-10-31"
-      }
+        term: 'express',
+        from: '2017-10-01',
+        to: '2017-10-31',
+      },
     };
 
     dateParser.parse(req.query, false);
 
     expect(req.query).to.deep.equal({
-      term: "express",
-      from: moment("2017-10-01").toDate(),
-      to: moment("2017-10-31").toDate()
+      term: 'express',
+      from: moment('2017-10-01').toDate(),
+      to: moment('2017-10-31').toDate(),
     });
 
     expect(req.query.from instanceof Date).to.equal(true);
@@ -46,43 +43,43 @@ describe('Date Parser', () => {
   });
 
   it('should parse the deep object with date params', () => {
-    let req = {
+    const req = {
       query: {
-        term: "express",
+        term: 'express',
         metaData: {
-          startDate: "2017-10-01 00:00:00"
-        }
-      }
+          startDate: '2017-10-01 00:00:00',
+        },
+      },
     };
 
     dateParser.parse(req.query);
 
     expect(req.query).to.deep.equal({
-      term: "express",
+      term: 'express',
       metaData: {
-        startDate: moment("2017-10-01").toDate()
-      }
+        startDate: moment('2017-10-01').toDate(),
+      },
     });
 
     expect(req.query.metaData.startDate instanceof Date).to.equal(true);
   });
 
   it('should parse the object with Array of dates', () => {
-    let req = {
+    const req = {
       query: {
-        term: "express",
-        dates: ["Friday, June 24, 2016 10:42 AM", "2017-08-11T00:00:00.000Z"]
-      }
+        term: 'express',
+        dates: ['Friday, June 24, 2016 10:42 AM', '2017-08-11T00:00:00.000Z'],
+      },
     };
 
     dateParser.parse(req.query);
 
     expect(req.query).to.deep.equal({
-      term: "express",
+      term: 'express',
       dates: [
-        moment("Friday, June 24, 2016 10:42 AM", "LLLL", true).toDate(),
-        moment("2017-08-11T00:00:00.000Z").toDate()
-      ]
+        moment('Friday, June 24, 2016 10:42 AM', 'LLLL', true).toDate(),
+        moment('2017-08-11T00:00:00.000Z').toDate(),
+      ],
     });
 
     expect(req.query.dates[0] instanceof Date).to.equal(true);
@@ -90,47 +87,49 @@ describe('Date Parser', () => {
   });
 
   it('should parse the milliseconds style date', () => {
-    let req = {
+    const req = {
       query: {
-        term: "express",
-        startDate: 1501781876406
-      }
+        term: 'express',
+        startDate: '1501781876406',
+        endDate: 1602053999999,
+      },
     };
 
     dateParser.parse(req.query);
 
     expect(req.query).to.deep.equal({
-      term: "express",
-      startDate: moment("2017-08-03T17:37:56.406Z").toDate()
+      term: 'express',
+      startDate: moment('2017-08-03T17:37:56.406Z').toDate(),
+      endDate: moment('2020-10-07T06:59:59.999Z').toDate(),
     });
 
     expect(req.query.startDate instanceof Date).to.equal(true);
   });
 
   it('should parse the unix style date', () => {
-    let req = {
+    const req = {
       query: {
-        term: "express",
-        startDate: 1501781876.406
-      }
+        term: 'express',
+        startDate: '1501781876.406',
+      },
     };
 
     dateParser.parse(req.query);
 
     expect(req.query).to.deep.equal({
-      term: "express",
-      startDate: moment("2017-08-03T17:37:56.406Z").toDate()
+      term: 'express',
+      startDate: moment('2017-08-03T17:37:56.406Z').toDate(),
     });
 
     expect(req.query.startDate instanceof Date).to.equal(true);
   });
 
-  it('should skip to parse the attribute if it donn\'t contains text date', () => {
-    let req = {
+  it("should skip to parse the attribute if it doesn't contains text date", () => {
+    const req = {
       query: {
-        term: "express",
-        start: 1501781876.406
-      }
+        term: 'express',
+        start: 1501781876.406,
+      },
     };
 
     dateParser.parse(req.query);
